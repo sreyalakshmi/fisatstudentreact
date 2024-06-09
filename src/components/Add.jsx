2eb2cb8200422b6cf209ebe3d3dc204de9c6dd7a
sreyalakshmi <sreyalakshmisaji@gmail.com>
2024-06-09 15:11:49 +0530

    fisatstudentreact created

---
+++ src/components/Add.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react'
+import Navbar from './Navbar'
+
+const Add = () => {
+    const[data,changedata]=useState({
+        "firstname":"",
+        "lastname":"",
+        "college":"",
+        "dob":"",
+        "course":"",
+        "mobile":"",
+        "email":"",
+        "address":""
+    })
+    const inputHandler=(event)=>{
+        changedata({...data,[event.target.name]:event.target.value})
+    }
+    const readvalue=()=>{
+        console.log(data)
+    }
+  return (
+    <div>
+        <Navbar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">first name</label>
+                            <input type="text" className="form-control"  name= "firstname" value={data.firstname} onChange={inputHandler}/>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">last name</label>
+                            <input type="text" className="form-control" name="lastname" value={data.lastname} onChange={inputHandler} />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">college</label>
+                            <input type="text" className="form-control" name="college" value={data.college} onChange={inputHandler}/>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">dob</label>
+                            <input type="date"  id="" className="form-contol" name= "dob"  value={data.dob} onChange={inputHandler} />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">course</label>
+                            <input type="text" className="form-control" name="course" value={data.course} onChange={inputHandler} />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">mobile</label>
+                            <input type="text" className="form-control" name="mobile" value={data.mobile} onChange={inputHandler} />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">email</label>
+                            <input type="text" className="form-control" name= "email" value={data.email} onChange={inputHandler}/>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">address</label>
+                            <input type="text" className="form-control" name="address" value={data.address} onChange={inputHandler}/>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn-success" onClick={readvalue}>submit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Add
---
+++ src/components/View.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react'
+import Navbar from './Navbar'
+import axios from 'axios'
+
+const View = () => {
+    const[data,changedata]=useState([])
+    const fetchData=()=>{
+        axios.get("https://anishpdm.github.io/dummy-api-new/student.json").then(
+            (response)=>{
+                changedata(response.data)
+            }
+        ).catch().finally()
+    }
+    useEffect(()=>{fetchData()},[])
+    return (
+        <div>
+            <Navbar/>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">firstname</th>
+                                    <th scope="col">lastname</th>
+                                    <th scope="col">college</th>
+                                    <th scope="col">dob</th>
+                                    <th scope="col">course</th>
+                                    <th scope="col">email</th>
+                                    <th scope="col">address</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {data.map(
+                                    (value,index)=>{
+                                        return <tr>
+                                        <td scope="row">{value.firstname}</td>
+                                        <td>{value.lastname}</td>
+                                        <td>{value.college}</td>
+                                        <td>{value.dob}</td>
+                                        <td>{value.course}</td>
+                                        <td>{value.email}</td>
+                                        <td>{value.address}</td>
+                                        
+                                    </tr>
+
+                                    }
+                                )}
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default View
